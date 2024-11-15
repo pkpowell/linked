9ab3614d84b70fea2e999d150c4e6e9da5483231
phil <p@pkpowell.net>
2024-11-15 10:41:40 +0100

    updates

--- list.go
+++ list.go
@@ -2,6 +2,7 @@ package linked
 
 import (
 	"iter"
+	"sync"
 )
 
 type NodeData interface {
@@ -13,12 +14,14 @@ type Node[T NodeData] struct {
 	D        T
 	next     *Node[T]
 	previous *Node[T]
+	mtx      *sync.RWMutex
 }
 
 type List[T NodeData] struct {
 	head   *Node[T]
 	tail   *Node[T]
 	length int
+	mtx    *sync.RWMutex
 }
 
 // NewList creates a new empty list
@@ -27,12 +30,16 @@ func NewList[T NodeData]() *List[T] {
 		head:   nil,
 		tail:   nil,
 		length: 0,
+		mtx:    &sync.RWMutex{},
 	}
 }
 
 // InsertBefore adds a new node before a given node
 func (list *List[T]) InsertBefore(data T, n *Node[T]) *Node[T] {
-	node := &Node[T]{D: data}
+	node := &Node[T]{D: data, mtx: &sync.RWMutex{}}
+	list.mtx.Lock()
+	defer list.mtx.Unlock()
+
 	if list.length == 0 {
 		list.head = node
 		list.tail = node
@@ -46,7 +53,10 @@ func (list *List[T]) InsertBefore(data T, n *Node[T]) *Node[T] {
 
 // InsertAfter adds a new node after a given node
 func (list *List[T]) InsertAfter(data T, n *Node[T]) *Node[T] {
-	node := &Node[T]{D: data}
+	node := &Node[T]{D: data, mtx: &sync.RWMutex{}}
+	list.mtx.Lock()
+	defer list.mtx.Unlock()
+
 	if list.length == 0 {
 		list.head = node
 		list.tail = node
@@ -61,7 +71,10 @@ func (list *List[T]) InsertAfter(data T, n *Node[T]) *Node[T] {
 
 // Prepend adds a new node to the beginning of the list
 func (list *List[T]) Prepend(data T) *Node[T] {
-	node := &Node[T]{D: data}
+	node := &Node[T]{D: data, mtx: &sync.RWMutex{}}
+	list.mtx.Lock()
+	defer list.mtx.Unlock()
+
 	switch list.length {
 	case 0:
 		// init new list, head and tail point to new node
@@ -84,7 +97,10 @@ func (list *List[T]) Prepend(data T) *Node[T] {
 
 // Append adds a new node to the end of the list
 func (list *List[T]) Append(data T) *Node[T] {
-	node := &Node[T]{D: data}
+	node := &Node[T]{D: data, mtx: &sync.RWMutex{}}
+	list.mtx.Lock()
+	defer list.mtx.Unlock()
+
 	switch list.length {
 	case 0:
 		// init new list, head and tail point to new node
@@ -135,6 +151,9 @@ func (list *List[T]) Get(id string) *Node[T] {
 
 // DeleteNode deletes a node from the list
 func (list *List[T]) DeleteNode(node *Node[T]) {
+	list.mtx.Lock()
+	defer list.mtx.Unlock()
+
 	switch list.length {
 	case 0:
 		return
